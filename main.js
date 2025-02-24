17193339f6cc66a4df552bb3220a311d39a92f64
Tiago <tcominesi@yahoo.com.br>
2025-02-24 15:18:15 -0300

    casas decimais na média

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function atualizaTabela(){
 function atualizarMediaFinal (){
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;  
 }
 
